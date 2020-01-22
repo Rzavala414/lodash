7c37aefa04c33ee21b6c15dbc8a0b77624bc7fa3
Roger Zavala <rogeliozavala15@gmail.com>
2020-01-21 21:18:44 -0800

    new method created inRange()

--- _.js
+++ _.js
@@ -3,6 +3,18 @@ const _ = {
         let clampedNum1 = Math.max(number,lower)
         let clampedNum2 = Math.min(clampedNum1,upper)
         return clampedNum2;
+    },
+    inRange(number,startVal,endVal){
+        if(endVal === undefined){
+            endVal = startVal;
+            startVal = 0;
+        } else if(startVal > endVal){
+            startValHolder = startVal;
+            startVal = endVal;
+            endVal = startValHolder;
+        } else if(number>= starVal && number <= endVal){
+            
+        }
     }
 }
 
